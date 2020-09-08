c36933efd3984251bc6f12e472d03ec614468ce8
DebadityaPal <debaditya.pal6@gmail.com>
2020-09-08 17:43:29 +0530

    One epoch of failed training GG

--- dev_function.py
+++ dev_function.py
@@ -48,8 +48,8 @@ def train(seq2sql_model,bert_model,model_optimizer,bert_tokenizer,bert_configs,p
 
             cnt += len(t)
 
-            # if iB > 2:
-            #     break
+            if iB > 2:
+                break
             # Get fields
             nlu, nlu_t, sql_i, sql_q, sql_t, tb, hs_t, hds = load_data.get_fields(t)
             # nlu  : natural language utterance
@@ -173,6 +173,7 @@ def train(seq2sql_model,bert_model,model_optimizer,bert_tokenizer,bert_configs,p
         acc_wv = cnt_wv / cnt
         acc_lx = cnt_lx / cnt
         acc_x = cnt_x / cnt
+        acc = [ave_loss, acc_sc, acc_sa, acc_wn, acc_wc, acc_wo, acc_wvi, acc_wv, acc_lx, acc_x]
 
         return acc
 
@@ -212,8 +213,8 @@ def test(seq2sql_model,bert_model,model_optimizer,bert_tokenizer,bert_configs,pa
         for iB, t in enumerate(tqdm(test_loader)):
             cnt += len(t)
 
-            # if iB > 2:
-            #     break
+            if iB > 2:
+                break
             # Get fields
             nlu, nlu_t, sql_i, sql_q, sql_t, tb, hs_t, hds = load_data.get_fields(t)
             # nlu  : natural language utterance
