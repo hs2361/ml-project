54632c7c9b34b10cd341062255fbd5f897541233
DebadityaPal <debaditya.pal6@gmail.com>
2020-09-08 13:57:30 +0530

    Silly error

--- dev_function.py
+++ dev_function.py
@@ -9,8 +9,10 @@ import infer_functions
 import torch
 import time
 from tqdm.notebook import tqdm
-
 import seq2sql_model_testing
+import torch_xla
+import torch_xla.core.xla_model as xm
+
 
 def train(seq2sql_model,bert_model,model_optimizer,bert_tokenizer,bert_configs,path_wikisql,train_loader):
 
