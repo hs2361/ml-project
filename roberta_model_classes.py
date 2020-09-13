9f1cdf77f8097ec547f9a4a624f35bc7574f39ed
DebadityaPal <debaditya.pal6@gmail.com>
2020-09-13 17:20:16 +0530

    Guotong inconsistenies

--- roberta_model_classes.py
+++ roberta_model_classes.py
@@ -61,7 +61,7 @@ class RobertaConfig(BertConfig):
 
     def __init__(self, pad_token_id=1, bos_token_id=0, eos_token_id=2, **kwargs):
         """Constructs RobertaConfig."""
-        super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token_id, eos_token_id=eos_token_id, **kwargs)
+        super().__init__(vocab_size=30522)
 
 
 class RobertaEmbeddings(BERTEmbeddings):
