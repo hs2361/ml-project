36756d26fb5eb62ca7337a04829181ea289bddd5
DebadityaPal <debaditya.pal6@gmail.com>
2020-10-10 15:43:47 +0530

    details

--- infer_functions.py
+++ infer_functions.py
@@ -6,69 +6,6 @@ import re
 import os
 re_ = re.compile(' ')
 
-def infer(nlu1,
-          table_id, headers, types, tokenizer, 
-          model, model_bert, bert_config, max_seq_length, num_target_layers,
-          beam_size=4):
-   
-    model.eval()
-    model_bert.eval()
-
-    # Get inputs
-    nlu = [nlu1]
-
-    nlu_t1 = infer_functions.sent_split(nlu1)
-    nlu_t = [nlu_t1]
-    hds = [headers]
-    hs_t = [[]]
-
-    data = {}
-    data['question_tok'] = nlu_t[0]
-    data['table_id'] = table_id
-    data['header'] = headers
-    data = [data]
-
-    tb = {}
-    tb['id'] = table_id
-    tb['header'] = headers
-    tb['types'] = types
-    tb = [tb]
-
-    tk = tokenizer
-
-    check = infer_functions.process(data, path_wikisql, tk)
-    knowledge = [check[0][1]]
-    header_knowledge = [check[0][2]]
-
-    wemb_n, wemb_h, l_n, l_hpu, l_hs, \
-    nlu_tt, t_to_tt_idx, tt_to_t_idx \
-        = bert_training.get_wemb_roberta(bert_config, model_bert, tokenizer, nlu_t, hds, max_seq_length,
-                        num_out_layers_n=num_target_layers, num_out_layers_h=num_target_layers)
-
-    prob_sca, prob_w, prob_wn_w, pr_sc, pr_sa, pr_wn, pr_sql_i = model.beam_forward(wemb_n, l_n, wemb_h, l_hpu,
-                                                                                    l_hs, tb,
-                                                                                    nlu_t, nlu_tt,
-                                                                                    tt_to_t_idx, nlu,
-                                                                                    beam_size=beam_size,
-                                                                                    knowledge=knowledge,
-                                                                                    knowledge_header=header_knowledge)
-    pr_wc, pr_wo, pr_wv, pr_sql_i = infer_functions.sort_and_generate_pr_w(pr_sql_i)
-    
-    
-    if len(pr_sql_i) != 1:
-        raise EnvironmentError
-    pr_sql_q1 = seq2sql_model_testing.generate_sql_q(pr_sql_i, tb)
-    pr_sql_q = [pr_sql_q1]
-
-    print(f'START ============================================================= ')
-    print(f'{hds}')
-    print(f'nlu: {nlu}')
-    print(f'pr_sql_i : {pr_sql_i}')
-    print(f'pr_sql_q : {pr_sql_q}')
-    print(f'---------------------------------------------------------------------')
-
-    return pr_sql_i
-
 def tokenize_corenlp_direct_version(client, nlu1):
     nlu1_tok = []
     for sentence in client.annotate(nlu1).sentence:
@@ -138,7 +75,7 @@ def sort_and_generate_pr_w(pr_sql_i):
 
     return pr_wc, pr_wo, pr_wv, pr_sql_i
 
-def process(data,model_path,tokenize,bert_model_type='uncased_L-12_H-768_A-12'):
+def process(data,tokenize):
     final_all = []
     badcase = 0
     for i, one_data in enumerate(data):
@@ -200,54 +137,14 @@ def process(data,model_path,tokenize,bert_model_type='uncased_L-12_H-768_A-12'):
                         # print("!!!!")
                         continue
 
-        # for row in one_table["rows"]:
-        #     for iiii, cell in enumerate(row):
-        #         cell = str(cell).lower()
-        #         flag, start_, end_ = contains2(re_.sub('', cell), "".join(one_data["question_tok"]).lower())
-        #         if flag == True:
-        #             final_header[iiii] = 2
-
         one_final["header_knowledge"] = final_header
 
-        # for row in one_table["rows"]:
-        #     for cell in row:
-        #         cell = str(cell).lower()
-        #         # cell = cell.replace('"',"")
-        #         cell_tokens = tokenize.tokenize(cell)
-
-
-
-        #         if len(cell_tokens)==0:
-        #             continue
-
-        #         flag, start_, end_ = contains2(re_.sub('', cell),  "".join(one_data["question_tok"]).lower())
-        #         # flag, start, end = contains(cell_tokens, nlu_tt1)
-        #         # if flag==False:
-        #         #     flag, start, end = contains(cell_tokens, nlu_tt2)
-        #         #     if len(nlu_tt1) != len(nlu_tt2):
-        #         #         continue
-        #         if flag == True:
-        #             try:
-        #                 start = t_to_tt_idx1[charindex2wordindex[start_]]
-        #                 end = t_to_tt_idx1[charindex2wordindex[end_]]
-        #                 for ii in range(start,end):
-        #                     final_question[ii] = 2
-        #                 final_question[start] = 1
-        #                 final_question[end] = 3
-        #                 one_final["bertindex_knowledge"] = final_question
-        #                 break
-        #             except:
-        #                 # print("!!!")
-        #                 continue
-        # if i%1000==0:
-        #     print(i)
         if "bertindex_knowledge" not in one_final and len(one_final["sql"]["conds"])>0:
-            # print(one_data["question"])
-            # print(one_table["rows"])
             one_final["bertindex_knowledge"] = [0] * len(nlu_tt1)
             badcase+=1
+            
         final_all.append([one_data["question_tok"],one_final["bertindex_knowledge"],one_final["header_knowledge"]])
-        # print(badcase)
+        print(badcase)
     return final_all
   
 
@@ -257,3 +154,66 @@ def contains2(small_str,big_str):
         return True,start,start+len(small_str)-1
     else:
         return False,-1,-1
+
+def infer(nlu1,
+          table_id, headers, types, tokenizer, 
+          model, model_roberta, roberta_config, max_seq_length, num_target_layers,
+          beam_size=4):
+   
+    model.eval()
+    model_roberta.eval()
+
+    # Get inputs
+    nlu = [nlu1]
+
+    nlu_t1 = sent_split(nlu1)
+    nlu_t = [nlu_t1]
+    hds = [headers]
+    hs_t = [[]]
+
+    data = {}
+    data['question_tok'] = nlu_t[0]
+    data['table_id'] = table_id
+    data['header'] = headers
+    data = [data]
+
+    tb = {}
+    tb['id'] = table_id
+    tb['header'] = headers
+    tb['types'] = types
+    tb = [tb]
+
+    tk = tokenizer
+
+    check = process(data, tk)
+    knowledge = [check[0][1]]
+    header_knowledge = [check[0][2]]
+
+    wemb_n, wemb_h, l_n, l_hpu, l_hs, \
+    nlu_tt, t_to_tt_idx, tt_to_t_idx \
+        = bert_training.get_wemb_roberta(roberta_config, model_roberta, tokenizer, nlu_t, hds, max_seq_length,
+                        num_out_layers_n=num_target_layers, num_out_layers_h=num_target_layers)
+
+    prob_sca, prob_w, prob_wn_w, pr_sc, pr_sa, pr_wn, pr_sql_i = model.beam_forward(wemb_n, l_n, wemb_h, l_hpu,
+                                                                                    l_hs, tb,
+                                                                                    nlu_t, nlu_tt,
+                                                                                    tt_to_t_idx, nlu,
+                                                                                    beam_size=beam_size,
+                                                                                    knowledge=knowledge,
+                                                                                    knowledge_header=header_knowledge)
+    pr_wc, pr_wo, pr_wv, pr_sql_i = sort_and_generate_pr_w(pr_sql_i)
+    
+    
+    if len(pr_sql_i) != 1:
+        raise EnvironmentError
+    pr_sql_q1 = seq2sql_model_testing.generate_sql_q(pr_sql_i, tb)
+    pr_sql_q = [pr_sql_q1]
+
+    print(f'START ============================================================= ')
+    print(f'{hds}')
+    print(f'nlu: {nlu}')
+    print(f'pr_sql_i : {pr_sql_i}')
+    print(f'pr_sql_q : {pr_sql_q}')
+    print(f'---------------------------------------------------------------------')
+
+    return pr_sql_i
